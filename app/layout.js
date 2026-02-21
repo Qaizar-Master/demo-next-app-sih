4f551ad44e12082129f51a3696cd6f5c14159038
Qaizar-Master <qaizar.master06@gmail.com>
2026-02-22 01:51:04 +0530

    layout updated

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,18 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { env } from "../lib/env";
 
 export const metadata = {
-  title: "EcoChamps - The Futue of Education",
+  title: "EcoChamps - The Future of Education",
   description: "Forming the Champs of tomorrow",
 };
 
 export default function RootLayout({ children }) {
-  const publishableKey = env.clerkPublishableKey;
   return (
-    <html lang="en">
-      <body>
-        {publishableKey ? (
-          <ClerkProvider publishableKey={publishableKey}>
-            {children}
-          </ClerkProvider>
-        ) : (
-          children
-        )}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
