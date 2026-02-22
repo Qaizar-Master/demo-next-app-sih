7b2a87e17415c33216946f3d8c65cb359a2db936
Qaizar-Master <qaizar.master06@gmail.com>
2026-02-22 06:54:13 +0530

    removed irrelevant pages and files

--- app/page.js
+++ app/page.js
@@ -1,10 +1,7 @@
-import { ClerkProvider } from "@clerk/nextjs";
 import LandingPage from "../components/Landing/LandingPage";
 
 export default function Home() {
   return (
-    <ClerkProvider>
       <LandingPage />
-    </ClerkProvider>
   )
 }
